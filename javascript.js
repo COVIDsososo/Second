3f2e8386d506d46ef12006d5034eb05edafb4da8
COVIDsososos <abkommem@gmail.com>
2020-04-25 20:28:14 +0400

    colorChange

--- javascript.js
+++ javascript.js
@@ -4,6 +4,16 @@ function onClick() {
   alert (clicks);
 }
 
+function changeColor() {
+  document.getElementById('background-color').href="./styles.css";
+  x=Math.floor((Math.random() * 255) + 1);
+  y=Math.floor((Math.random() * 255) + 1);
+  z=Math.floor((Math.random() * 255) + 1);
+  result = (x, y, z)
+  background-color = RGB result;
+}
 
+
+//hhhh
 
 
